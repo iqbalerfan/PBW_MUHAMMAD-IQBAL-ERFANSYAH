81d9f096a7c186f9d6e08bba9aa8509e1e47eb77
Iqbal Erfansyah <145417561+iqbalerfan@users.noreply.github.com>
2025-03-27 19:17:38 +0700

    Add files via upload

---
+++ praktikum/praktikum 5/main.js
@@ -0,0 +1,70 @@
+// alert('halo wasap gengs') // pop up
+// console.log('ini sebuah console') // inspect
+
+// function contohLet() {
+//     let y = 10;
+//     if (true) {
+//         let y = 20;
+//         console.log("Nilai dalam blok:", y);
+//     }
+//     console.log("Nilai luar blok:", y);
+// }
+
+// contohLet();
+
+// let nama = 'Nur Salamah Azzahrah'; // string
+// let usia = 20; // integer
+// let tinggiBadan = 148.8; // double atau float
+// let beratBadan;
+// beratBadan = 43.5; // double atau float
+// let uang = null;
+
+// if (uang == null) {
+//     uang = "belum ada";
+// } else {
+//     uang = "udah ada";
+// }
+
+// switch (uang) {
+//     case 1:
+//         uang = "punya sedikit";
+//         break;
+//     case 2:
+//         uang = "punya cukup";
+//         break;
+//     default:
+//         uang = "tidak ada";
+//         break;
+// }
+
+// let uangAwal = 20000;
+// let uangTambahan = 15000;
+// const uangTotal = uangAwal - uangTambahan; // aritmatika
+
+// alert('nama saya ' + nama) // pop up
+// alert(`nama saya ${nama}, usia saya ${usia} tahun, tinggi badan saya ${tinggiBadan} cm`);
+
+// alert(`uang awal gue sebesar Rp.${uangAwal} dan gue dikasih tambahan Rp.${uangTambahan}, jadi totalnya Rp.${uangTotal}`);
+
+// let namaPanggilan = [
+//     'Nur',  // index 0
+//     'Ara',  // index 1
+//     'Nuy'   // index 2
+// ];
+
+// let mahasiswa = {
+//     nama: "Rina",
+//     usia: 20,
+//     jurusan: "Informatika"
+// }; // Object
+
+// console.log(mahasiswa.jurusan);
+
+// namaPanggilan.push('Nun', 'Zahra'); // push: menambahkan isi array di akhir
+// namaPanggilan.shift(); // shift: menghapus salah satu index array dari awal
+// namaPanggilan.pop(); // pop: menghapus salah satu index array dari akhir
+
+// alert(namaPanggilan);
+
+// console.log(namaPanggilan.length); // menampilkan panjang array
+
